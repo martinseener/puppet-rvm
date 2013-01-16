f57d0dd237ef39f69e00f3a6549a38d44724a0c1
Martin Seener <martin.seener@barzahlen.de>
2013-01-16 15:01:18 +0100

    updated to latest 1.17.9 rvm version (tested)

--- manifests/packages/common.pp
+++ manifests/packages/common.pp
@@ -9,9 +9,9 @@ class rvm::packages::common {
     unless  => 'which rvm',
   }
   # install-rvm command enhanced to use a specific rvm version
-  # chosen 1.17.8 which is latest stable on Jan 11, 2013 and recommended for server usage
+  # chosen 1.17.9 which is latest stable on Jan 16, 2013 and recommended for server usage
   exec { 'install-rvm':
-    command => "bash /tmp/rvm --version 1.17.8",
+    command => "bash /tmp/rvm --version 1.17.9",
     creates => '/usr/local/rvm/bin/rvm',
     require => Exec['download-rvm-install'],
   }
