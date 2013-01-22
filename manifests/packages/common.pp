755dda0909deb47fb85e8d8c551fbe352a1f6d15
Martin Seener <martin.seener@barzahlen.de>
2013-01-22 18:35:09 +0100

    added --no-check-certificate because raw.githubs ssl certs CN doesnt match and wget denies download

--- manifests/packages/common.pp
+++ manifests/packages/common.pp
@@ -4,7 +4,7 @@ class rvm::packages::common {
   }
   
   exec { 'download-rvm-install':
-    command => 'wget -O /tmp/rvm https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer',
+    command => 'wget --no-check-certificate -O /tmp/rvm https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer',
     creates => '/tmp/rvm',
     unless  => 'which rvm',
   }
