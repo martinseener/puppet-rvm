9d7218d66bae2d8840ba53ecc8c6b18c68f79360
Martin Seener <martin.seener@barzahlen.de>
2015-01-08 14:26:22 +0100

    Newer RVM requires GPG key checks, so this is also added now

--- manifests/packages/common.pp
+++ manifests/packages/common.pp
@@ -11,13 +11,19 @@ class rvm::packages::common {
   # install-rvm command enhanced to use a specific rvm version
   # 1.25.22 at Mar, 26, 2014 and recommended for server usage
   # 1.26.9 at 08.01.2015
+  exec { 'get-rvm-key':
+    command => "curl -sSL https://rvm.io/mpapis.asc | gpg --import -",
+    requires => Exec['download-rvm-install'],
+  }
+
   exec { 'install-rvm':
     command => "bash /tmp/rvm --version 1.26.9",
     creates => '/usr/local/rvm/bin/rvm',
-    require => Exec['download-rvm-install'],
+    require => Exec['get-rvm-key'],
   }
   file { '/tmp/rvm':
     ensure  => absent,
     require => Exec['install-rvm'],
   }
 }
+
